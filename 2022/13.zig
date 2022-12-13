db38f7a0f3cd149278d9d87d6bdad6f8f1a9cf7e
Frechdachs <frechdachs@rekt.cc>
2022-12-13 18:25:08 +0100

    Day 13: Improvements

--- 2022/13.zig
+++ 2022/13.zig
@@ -18,84 +18,20 @@ const TEST_INPUT_PATH = "input/13test";
 
 
 const Parsed = struct {
-    packets: []Packet,
+    packets: [][]const u8,
     allocator: Allocator,
 
     fn deinit(self: *@This()) void {
-        for (self.packets) |*p| {
-            p.deinit();
-        }
-        self.allocator.free(self.packets);
-    }
-};
-
-const Packet = struct {
-    value: i8,
-    packets: []Packet,
-    str: []const u8,
-    allocator: Allocator,
-
-    const Self = @This();
-
-    fn init(allocator: Allocator, raw: []const u8) Self {
-        var packets = List(Packet).init(allocator);
-        var i: usize = 1;
-        outer: while (i < raw.len - 1) {
-            const c = raw[i];
-            if (c == '[') {
-                var counter: usize = 1;
-                var j: usize = i + 1;
-                while (j < raw.len - 1) : (j += 1) {
-                    if (raw[j] == '[') {
-                        counter += 1;
-                    } else if (raw[j] == ']') {
-                        counter -= 1;
-                        if (counter == 0) {
-                            packets.append(Packet.init(allocator, raw[i..j + 1])) catch unreachable;
-                            i = j + 1;
-                            continue :outer;
-                        }
-                    }
-                }
-                unreachable;
-            } else if (c >= '0' and c <= '9') {
-                var l = List(Packet).init(allocator);
-                var value = @intCast(i8, c - '0');
-                while (raw[i + 1] >= '0' and raw[i + 1] <= '9') : (i += 1) {
-                    value = value * 10 + @intCast(i8, raw[i + 1] - '0');
-                }
-                packets.append(.{
-                    .value = value,
-                    .packets = l.toOwnedSlice(),
-                    .str = "",
-                    .allocator = allocator,
-                    }) catch unreachable;
-            }
-            i += 1;
-        }
-
-        return .{
-            .value = -1,
-            .packets = packets.toOwnedSlice(),
-            .str = raw,
-            .allocator = allocator,
-        };
-    }
-
-    fn deinit(self: *Self) void {
-        for (self.packets) |*p| {
-            p.deinit();
-        }
         self.allocator.free(self.packets);
     }
 };
 
 fn parseInput(allocator: Allocator, raw: []const u8) Parsed
 {
-    var packets = List(Packet).init(allocator);
+    var packets = List([]const u8).init(allocator);
     var it = tokenize(u8, raw, "\n");
     while (it.next()) |line| {
-        packets.append(Packet.init(allocator, line)) catch unreachable;
+        packets.append(line) catch unreachable;
     }
 
     return .{
@@ -111,7 +47,9 @@ fn part1(parsed: Parsed) usize
     while (i + 1 < parsed.packets.len) : (i += 2) {
         const p1 = parsed.packets[i];
         const p2 = parsed.packets[i + 1];
-        const check = comparePackets(p1, p2);
+        var idx1: usize = 0;
+        var idx2: usize = 0;
+        const check = comparePackets(p1, p2, &idx1, &idx2);
         if (check == .lt) accum += i / 2 + 1;
     }
 
@@ -120,65 +58,97 @@ fn part1(parsed: Parsed) usize
 
 fn part2(parsed: Parsed) usize
 {
-    var packets = List(Packet).init(parsed.allocator);
-    defer packets.deinit();
-    packets.appendSlice(parsed.packets) catch unreachable;
-    var p1 = Packet.init(parsed.allocator, "[[2]]");
-    defer p1.deinit();
-    var p2 = Packet.init(parsed.allocator, "[[6]]");
-    defer p2.deinit();
-    packets.append(p1) catch unreachable;
-    packets.append(p2) catch unreachable;
+    const divider1 = "[[2]]";
+    const divider2 = "[[6]]";
 
-    sort(Packet, packets.items, {}, lessThanPackets);
+    var accum1: usize = 1;
+    var accum2: usize = 1;
+    for (parsed.packets) |p| {
+        var idx1: usize = 0;
+        var idx2: usize = 0;
 
-    var accum: usize = 1;
-    for (packets.items) |p, i| {
-        if (std.mem.eql(u8, p.str, "[[2]]")) {
-            accum *= i + 1;
-        } else if (std.mem.eql(u8, p.str, "[[6]]")) {
-            accum *= i + 1;
+        if (comparePackets(p, divider1, &idx1, &idx2) == .lt) {
+            accum1 += 1;
+        } else {
+            idx1 = 0;
+            idx2 = 0;
+            if (comparePackets(p, divider2, &idx1, &idx2) == .lt)
+                accum2 += 1;
         }
     }
 
-    return accum;
+    return accum1 * (accum1 + accum2);
 }
 
-fn lessThanPackets(context: void, p1: Packet, p2: Packet) bool
+fn comparePackets(p1: []const u8, p2: []const u8, idx1: *usize, idx2: *usize) std.math.Order
 {
-    _ = context;
+    var c1 = p1[idx1.*];
+    var c2 = p2[idx2.*];
 
-    return comparePackets(p1, p2) == .lt;
-}
+    const p1_is_number = c1 != '[';
+    const p2_is_number = c2 != '[';
 
-fn comparePackets(p1: Packet, p2: Packet) std.math.Order
-{
-    if (p1.packets.len == 0 and p2.packets.len == 0) {
-        return std.math.order(p1.value, p2.value);
+    if (p1_is_number and p2_is_number) {
+        const n1 = getNumber(p1, idx1);
+        const n2 = getNumber(p2, idx2);
+        return std.math.order(n1, n2);
 
-    } else if (p2.packets.len == 0) {
-        var comp = p1.packets[0];
-        while (comp.packets.len != 0) {
-            comp = comp.packets[0];
-        }
-        return if (comp.value < p2.value) .lt else .gt;
+    } else if (p2_is_number) {
+        idx1.* += 1;
+        const n1 = unpackOne(p1, idx1);
+        const n2 = getNumber(p2, idx2);
+        return if (n1 < n2) .lt else .gt;
 
-    } else if (p1.packets.len == 0) {
-        var comp = p2.packets[0];
-        while (comp.packets.len != 0) {
-            comp = comp.packets[0];
-        }
-        return if (p1.value <= comp.value) .lt else .gt;
+    } else if (p1_is_number) {
+        idx2.* += 1;
+        const n1 = getNumber(p1, idx1);
+        const n2 = unpackOne(p2, idx2);
+        return if (n1 <= n2) .lt else .gt;
 
     } else {
-        var i: usize = 0;
-        while (i < @min(p1.packets.len, p2.packets.len)) : (i += 1) {
-            const check = comparePackets(p1.packets[i], p2.packets[i]);
-            if (check != .eq) return check;
+        idx1.* += 1;
+        idx2.* += 1;
+
+        var check = std.math.Order.eq;
+        while (check == .eq) {
+            c1 = p1[idx1.*];
+            c2 = p2[idx2.*];
+            const p1_is_end = c1 == ']';
+            const p2_is_end = c2 == ']';
+            if (p1_is_end or c1 == ',') idx1.* += 1;
+            if (p2_is_end or c2 == ',') idx2.* += 1;
+
+            if (p1_is_end and p2_is_end) return .eq;
+            if (p1_is_end) return .lt;
+            if (p2_is_end) return .gt;
+
+            check = comparePackets(p1, p2, idx1, idx2);
         }
-        if (p1.packets.len == p2.packets.len) return .eq;
-        return if (i < p2.packets.len) .lt else .gt;
+
+        return check;
     }
+}
+
+fn getNumber(p: []const u8, idx: *usize) i8
+{
+    var i: usize = idx.* + 1;
+    while (p[i] >= '0' and p[i] <= '9') i += 1;
+    const num = parseInt(i8, p[idx.*..i], 10) catch unreachable;
+    idx.* = i;
+
+    return num;
+}
+
+fn unpackOne(p: []const u8, idx: *usize) i8
+{
+    var c = p[idx.*];
+    while (c == '[') {
+        idx.* += 1;
+        c = p[idx.*];
+    }
+    if (c == ']') return -1;
+
+    return getNumber(p, idx);
 }
 
 pub fn main() !void
@@ -229,7 +199,7 @@ fn benchmark() !void
     i = 0;
     var p1: usize = undefined;
     var part1_time: u64 = 0;
-    while (i < 1000 + warmup) : (i += 1) {
+    while (i < 10000 + warmup) : (i += 1) {
         parsed = parseInput(allocator, input);
         defer parsed.deinit();
         if (i >= warmup) timer.reset();
@@ -243,7 +213,7 @@ fn benchmark() !void
     i = 0;
     var p2: usize = undefined;
     var part2_time: u64 = 0;
-    while (i < 1000 + warmup) : (i += 1) {
+    while (i < 10000 + warmup) : (i += 1) {
         parsed = parseInput(allocator, input);
         defer parsed.deinit();
         if (i >= warmup) timer.reset();
